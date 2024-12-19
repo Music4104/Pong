08aa2da3bc26183a12efa72b442a6df2d9cf944f
Music4104 <gjamy04@gmail.com>
2024-12-19 14:55:13 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,14 +52,14 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("rain15.mp3");
+export var ambientSound = new soundFile("silence.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
+ambientSound.player.loop = -20 
 
-export var adventureMusic = new soundFile("silence.mp3");
+export var adventureMusic = new soundFile("rain15.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
